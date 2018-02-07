761ed49cb0c31326a3c7b9cb3ecb69ab5ac8de13
MatJackson <mjackson74u@gmail.com>
2018-02-07 00:41:13 -0500

    app.js addition setup, basic frameworks for models and routes established.

--- app.js
+++ app.js
@@ -1,34 +1,93 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/db2');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init app
 var app = express();
 
-/* example of middleware
-var logger = function(req, res, next){
-    console.log('Logging...');
-    next();
-}
-app.use(logger);
-*/
 
-//view engine
+// View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-//body parser middleware
+// BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 
-//set static path
-app.use(express.static(path.join(__dirname, 'public')))
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator - taken from git page
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.');
+        var root = namespace.shift();
+        var formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error'); // for passport
+    res.locals.user = req.user || null // track if user logged in/out
+    next();
+});
+
+// Middleware for Route Files
+app.use('/', routes);
+app.use('/users', users);
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+
+// What's this for? ask Steph -MJ
 app.get('/', function(req,res){
     res.render('index');
-})
+});
 
 app.listen(3000, function(){
-    console.log('server started on port 3000....');
-})
+    console.log('server started on port '+app.get('port'));
+});
