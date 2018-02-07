6d43e6c3d49005369b956230ad6327aac5b5f70f
MatJackson <mjackson74u@gmail.com>
2018-02-07 00:41:13 -0500

    app.js addition setup, basic frameworks for models and routes established.

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var User = require('../models/user');
+
+// Register
+
+// Login
+
+module.exports = router;
