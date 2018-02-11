cd02c7a27640f7c9567c186d9c0b5a291623c880
Tommy Mei <tommymei618@hotmail.com>
2018-02-11 10:30:43 -0500

    #24 code refactoring, show.ejs name changed to postpage for consistency

--- routes/comments.js
+++ routes/comments.js
@@ -1,41 +1,38 @@
 var express = require("express");
 var router = express.Router();
-var Post = require("../models/post");
+var Post = require("../models/posts");
 var Comment = require ("../models/comment");
 
-//========================================
+//===================
 //COMMENT ROUTES
-router.get("/posts/:id/comments/new",isLoggedIn, function(req,res){
-	Post.findById(req.params.id,function(err,post){
-		if(err)
-		console.log(err);
-		else{
-			res.render("comments/new",{post:post});
-		}
-	});
+//===================
 
-});
-//COMMENTS CREATE
-router.post("/posts/:id/comments", function(req,res){
+//COMMENTS CREATE ROUTE
+router.post("/post/:id/user/:userid/:title/comments", isLoggedIn, function(req,res){
 	Post.findById(req.params.id,function(err,post){
 		if(err){
 		console.log(err);
-		res.redirect("/posts");
+		//error message for time being
+		res.send("Cannot find post");
 		}
 		else{
-			//console.log(req.body.comment);
+			
 			Comment.create(req.body.comment,function(err,comment){
 				if(err){
 					console.log(err);
 
 				}
 				else{
+					var currentDate = datetime.create();
+					var formattedDate = currentDate.format('Y-m-d H:M:S');
+					
 					comment.author.id=req.user._id;
 					comment.author.username = req.user.username;
+					comment.timePosted = formattedDate;
 					comment.save();
 					post.comments.push(comment._id);
 					post.save();
-					res.redirect("/posts/"+post._id);
+					res.redirect("/post/"+post._id+"/user/" + post.author._id +"/"+ post.title);
 				}
 			});
 		}
