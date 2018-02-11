25a79641785618bee1d09e26b348f809e1753c98
Tommy Mei <tommymei618@hotmail.com>
2018-02-11 10:30:43 -0500

    #24 code refactoring, show.ejs name changed to postpage for consistency

--- models/comment.js
+++ models/comment.js
@@ -8,7 +8,12 @@ var commentSchema = mongoose.Schema(
                 ref:"User"
             },
             username: String
-        }
+        },
+        // @Nath, upvotes and downvotes are present for the time being, feel free to change the model
+        upvotes: Number,
+        downvotes: Number,
+        timePosted: String
+
     });
 
     module.exports = mongoose.model("Comment",commentSchema);
