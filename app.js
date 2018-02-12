6e66651c696921c271e595d8d76677a89c0d69a3
Zhen Yee <Zhen@zhens-mbp.wireless.concordia.ca>
2018-02-12 14:54:10 -0500

    added nodemon package and title #11

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.listen(3000, function(){
-    console.log('server started on port 3000....');
+    console.log('Server started on port 3000....');
 });
