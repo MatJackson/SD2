18bf142d63acc5f20945ac5cf135e1f19de47080
Zhen Yee <Zhen@Zhens-MacBook-Pro.local>
2018-02-15 15:28:29 -0500

    Updated code to work with reply and logged in #11

--- models/posts.js
+++ models/posts.js
@@ -1,19 +1,29 @@
 var mongoose = require('mongoose');
 
 var postSchema = mongoose.Schema({
-    id: String,
+    
     title: String,
     description: String,
     author: {
-        userName: String,
-        id: String,
+        id:{ 
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        username:String
     },
     comments:[{
 		type: mongoose.Schema.Types.ObjectId,
 		ref: "Comment"
     }],
-    upvotes: Number,
+    like: {
+        type: Number, 
+        default: 0
+    },
+    dislike: {
+        type: Number, 
+        default: 0
+    },
     timePosted: String
-}, {collection: 'posts'});
+});
 
 module.exports = mongoose.model('Post', postSchema);;
