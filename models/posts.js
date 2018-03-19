60d4794632ae29fb71bdf021a77bf0f28efb5d87
JeffreyLi16 <jeffreyli16@hotmail.com>
2018-03-19 17:02:04 -0400

    JavaScript refactoring #78
    Refactored every .js file to follow JavaScript Standard Style.

--- models/posts.js
+++ models/posts.js
@@ -1,43 +1,43 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
 
 var postSchema = mongoose.Schema({
-    
-    title: String,
-    description: String,
-    author: {
-        id:{ 
-            type: mongoose.Schema.Types.ObjectId,
-            ref:"User"
-        },
-        username:String
-    },
-    comments:[{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Comment"
-    }],
-    like: {
-        type: Number, 
-        default: 0
-    },
-    dislike: {
-        type: Number, 
-        default: 0
-    },
-    voteArray: [{
-        userID:{ 
-            type: String
-        },    
-        didLike: {
-            type: Boolean
-        },
-        didDislike: {
-            type: Boolean
-        }
-    }],
 
-    timePosted: String
-});
+  title: String,
+  description: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    username: String
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  like: {
+    type: Number,
+    default: 0
+  },
+  dislike: {
+    type: Number,
+    default: 0
+  },
+  voteArray: [{
+    userID: {
+      type: String
+    },
+    didLike: {
+      type: Boolean
+    },
+    didDislike: {
+      type: Boolean
+    }
+  }],
 
-postSchema.index({'$**': 'text'});
+  timePosted: String
+})
 
-module.exports = mongoose.model('Post', postSchema);;
+postSchema.index({'$**': 'text'})
+
+module.exports = mongoose.model('Post', postSchema)
--- test/test.js
+++ test/test.js
@@ -1,20 +1,17 @@
-var request = require("request"),
-    assert = require('assert'),
-    sd2 = require("../app.js"),
-    base_url = "http://localhost:3000/";
+var request = require('request')
+var assert = require('assert')
+var sd2 = require('../app.js')
+var base_url = 'http://localhost:3000/'
 
-describe("SD2 Test", function() {
-  describe("GET /", function() {
-    it("returns status code 200", function() {
-
-      request.get(base_url, function(error, response, body) {
-
-        assert.equal(200, response.statusCode);
-        sd2.closeServer();
-        done();
-
-      });
-
-    }); //you can add other it() for other tests
-  });
-});
+describe('SD2 Test', function () {
+  describe('GET /', function () {
+    it('returns status code 200', function () {
+      request.get(base_url, function (error, response, body) {
+        if (error) throw error
+        assert.equal(200, response.statusCode)
+        sd2.closeServer()
+        done()
+      })
+    }) // you can add other it() for other tests
+  })
+})
